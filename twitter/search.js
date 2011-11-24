ce3c6d1fea318f4174bd06123ad21f047f529288
kouki yamada <yamakk@gmail.com>
2011-11-24 11:00:21 +0900

    add search.js connect to cache server x42.

--- twitter/search.js
+++ twitter/search.js
@@ -7,16 +7,15 @@ var student_id = "";
 var canvas;
 var context;
 
-//ユーザ名を引数に与えると、Twitterのつぶやきを取ってくる
-function requestTweet(username){
+//キーワードを引数に与えると、キーワードを含むつぶやきを最大100件まで取ってくる
+function requestSearch(keyword){
 
     // 学内用 API制限を回避するためTwitterへのリクエストを1時間保存する
-    var url = "http://124.35.44.42:5445/main/tweet?screen_name=" +
-    username+"&student_id="+student_id+"&callback=?";
+    var url = "http://124.35.44.42:5445/main/search?keyword=" +
+	keyword+"&student_id="+student_id+"&callback=?";
 
     //学外用 直接Twitterへリクエストする (将来はこちらを使うべき)
-    //var url = "https://api.twitter.com/1/statuses/user_timeline.json?screen_name=" +
-    //username + "&count=200&callback=?";
+    //var url = "http://search.twitter.com/search.json?q="+keyword+"&rpp=100&callback=?";
 
     console.log("request to "+url);
     $.getJSON(url, getTweet);
@@ -24,29 +23,30 @@ function requestTweet(username){
 
 //requestTweetがTwitterからデータを受け取ると呼び出される
 function getTweet(json){
-    for(var i=0; i<json.length; i++){
-	var date = new Date(json[i].created_at);
+    for(var i=0; i<json.results.length; i++){
+	var date = new Date(json.results[i].created_at);
 	// つぶやきの文字列をtweetにいれる
-	var tweet = json[i].text;
+	var tweet = json.results[i].text;
+	// つぶやいたユーザの名前をscreen_nameにいれる
+	var screen_name = json.results[i].from_user;
 	// つぶやきの作成時(0〜23)を hourにいれる
 	var hour = date.getHours();
 	console.log("No."+i);
 	console.log("つぶやき本文:"+tweet);
 	console.log("時間帯:"+hour);
+	console.log("ユーザ名:"+screen_name);
 	console.log("--------");
-
-	 var alpha = hour / 23.0;
-	 // 色の設定:不透明度をつぶやかれた時間によって
-	 context.fillStyle = "rgba(0, 0, 0, "+alpha+")";
-	 // 四角形の描画
-	 context.fillRect(i+3*i, 0 , 3, 600);
-
+	var alpha = hour / 23.0;
+	// 色の設定:不透明度をつぶやかれた時間によって
+	context.fillStyle = "rgba(0, 0, 0, "+alpha+")";
+	// 四角形の描画
+	context.fillRect(i+3*i, 0 , 3, 600);
     };
 };
 
 $(document).ready(function(){
    canvas = document.getElementById("mycanvas");
    context = canvas.getContext("2d");
-   requestTweet("earthquake_jp");
+   requestSearch("新潟");
 });
 
